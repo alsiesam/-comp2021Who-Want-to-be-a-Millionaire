3bf1b9fd1d19e9ee6b7fed486e68e2ecce8012ca
poonvanessa <iamvanessapoon@gmail.com>
2017-04-28 22:06:23 +0800

    Uploading the project progress

---
+++ millionaire.php
@@ -0,0 +1,22 @@
+#!/usr/local/bin/php
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/millionaire.css">
+</head>
+
+<body>
+    <div class="game">
+    
+iScripts NetMenus is a total online ordering solution that allows you to instantly create an online food court. This multi restaurant online ordering system allow users to search restaurants, customize orders, and have it delivered to their...
+iScripts NetMenus is a total online ordering solution that allows you to instantly create an online food court. This multi restaurant online ordering system allow users to search restaurants, customize orders, and have it delivered to their...
+iScripts NetMenus is a total online ordering solution that allows you to instantly create an online food court. This multi restaurant online ordering system allow users to search restaurants, customize orders, and have it delivered to their...
+iScripts NetMenus is a total online ordering solution that allows you to instantly create an online food court. This multi restaurant online ordering system allow users to search restaurants, customize orders, and have it delivered to their...
+    
+    </div>
+
+
+</body>
+
+</html>
